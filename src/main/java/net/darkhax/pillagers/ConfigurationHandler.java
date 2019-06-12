d40f77dd86c53b73b2901c87b1cfd335f4983ebe
Tyler Hancock <darklime@live.ca>
2019-06-12 04:52:11 -0600

    Slightly improved the code comments. Also some minor formatting changes.

--- src/main/java/net/darkhax/pillagers/ConfigurationHandler.java
+++ src/main/java/net/darkhax/pillagers/ConfigurationHandler.java
@@ -10,7 +10,7 @@ public class ConfigurationHandler {
     public static float basePercent = 0.05f;
     public static float lootingPercent = 0.015f;
     public static boolean allowFakePlayers = false;
-    
+
     public ConfigurationHandler (String file) {
 
         config = new Configuration(new File(file));
@@ -18,11 +18,11 @@ public class ConfigurationHandler {
     }
 
     private void syncConfigData () {
-    	
-    	basePercent = config.getFloat("basePercentage", Configuration.CATEGORY_GENERAL, 0.05f, 0f, 1f, "The base chance that a player will get villager loot when killing a villager.");
-    	lootingPercent = config.getFloat("lootingPercent", Configuration.CATEGORY_GENERAL, 0.015f, 0f, 1f, "The amount each level of looting contributes to a villager loot drop chance.");
-    	allowFakePlayers = config.getBoolean("allowFakePlayers", Configuration.CATEGORY_GENERAL, false, "When enabled, blocks like mob grinders and spikes which allow player kills to be automated will be able to automate villager loot drops.");
-    	
+
+        basePercent = config.getFloat("basePercentage", Configuration.CATEGORY_GENERAL, 0.05f, 0f, 1f, "The base chance that a player will get villager loot when killing a villager.");
+        lootingPercent = config.getFloat("lootingPercent", Configuration.CATEGORY_GENERAL, 0.015f, 0f, 1f, "The amount each level of looting contributes to a villager loot drop chance.");
+        allowFakePlayers = config.getBoolean("allowFakePlayers", Configuration.CATEGORY_GENERAL, false, "When enabled, blocks like mob grinders and spikes which allow player kills to be automated will be able to automate villager loot drops.");
+
         if (config.hasChanged()) {
             config.save();
         }
--- src/main/java/net/darkhax/pillagers/Pillagers.java
+++ src/main/java/net/darkhax/pillagers/Pillagers.java
@@ -20,58 +20,59 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod(modid = "pillagers", name = "Pillagers", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.3.526,);", certificateFingerprint = "@FINGERPRINT@")
 public class Pillagers {
 
-    public static ConfigurationHandler config = new ConfigurationHandler("config/pillagers.cfg");
+    private static ConfigurationHandler config = new ConfigurationHandler("config/pillagers.cfg");
 
     @EventHandler
     public void onPreInit (FMLPreInitializationEvent event) {
 
         MinecraftForge.EVENT_BUS.register(this);
     }
-    
+
     @SubscribeEvent
     public void onMobKilled (LivingDropsEvent event) {
-        
-    	// Checks if the initial conditions are right.
-        if (MathsUtils.tryPercentage(ConfigurationHandler.basePercent + (ConfigurationHandler.lootingPercent * event.getLootingLevel())) && event.getEntityLiving() instanceof EntityVillager && event.getSource().getTrueSource() != null) {
-            
-        	// Checks if the kill was a player kill.
-        	if (ConfigurationHandler.allowFakePlayers ? PlayerUtils.isPlayerDamage(event.getSource()) : PlayerUtils.isPlayerReal(event.getSource().getTrueSource())) {
-        		
+
+        // Checks if the initial conditions are right. These conditions include a configurable probability based on the looting enchantment, the entity being a villager, and the damage source being from an entity.
+        if (MathsUtils.tryPercentage(ConfigurationHandler.basePercent + ConfigurationHandler.lootingPercent * event.getLootingLevel()) && event.getEntityLiving() instanceof EntityVillager && event.getSource().getTrueSource() != null) {
+
+            // A secondary check to see if the damage was from a player. This check can be bypassed using the configuration file.
+            if (ConfigurationHandler.allowFakePlayers ? PlayerUtils.isPlayerDamage(event.getSource()) : PlayerUtils.isPlayerReal(event.getSource().getTrueSource())) {
+
                 final EntityVillager villager = (EntityVillager) event.getEntityLiving();
                 final EntityPlayer player = (EntityPlayer) event.getSource().getTrueSource();
-                
+
                 // No looting kids!
-                if (villager.isChild()) {
-                    
-                    return;
-                }
-                
-                final MerchantRecipeList trades = villager.getRecipes(player);
-                
-                if (!trades.isEmpty()) {
-                	
-                    ItemStack item = new ItemStack(Items.EMERALD);
-                    int attempts = 0;
-                    
-                    // Makes three attempts to find a valid recipe.
-                    while (attempts < 3) {
-                        
-                    	// Gets a random recipe from the villager's trades with the player.
-                        final MerchantRecipe recipe = trades.get(Constants.RANDOM.nextInt(trades.size()));
-                        
-                        // Recipe can't be disabled, and must have a buying item and a selling item.
-                        if (!recipe.isRecipeDisabled() && !recipe.getItemToSell().isEmpty() && !recipe.getItemToBuy().isEmpty()) {
-                            
-                            item = recipe.getItemToSell().copy();
-                            break;
+                if (!villager.isChild()) {
+
+                    final MerchantRecipeList trades = villager.getRecipes(player);
+
+                    // Ensure the villager actually has trades to offer.
+                    if (!trades.isEmpty()) {
+
+                        ItemStack item = new ItemStack(Items.EMERALD);
+                        int attempts = 0;
+
+                        // Makes three attempts to find a valid recipe.
+                        while (attempts < 3) {
+
+                            // Gets a random recipe from the villager's trades with the player.
+                            final MerchantRecipe recipe = trades.get(Constants.RANDOM.nextInt(trades.size()));
+
+                            // Recipe can't be disabled, and must have a buying item and a selling
+                            // item.
+                            if (!recipe.isRecipeDisabled() && !recipe.getItemToSell().isEmpty() && !recipe.getItemToBuy().isEmpty()) {
+
+                                item = recipe.getItemToSell().copy();
+                                break;
+                            }
+
+                            attempts++;
                         }
-                        
-                        attempts++;
+
+                        // Add the item to the list of EntityItem spawned into the world.
+                        EntityUtils.addDrop(item, event);
                     }
-                    
-                    EntityUtils.addDrop(item, event);
                 }
-        	}
+            }
         }
     }
 }
